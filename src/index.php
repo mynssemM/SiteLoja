3fb9e9da0b96e4e0bcfe59ff50f4550ff0eac39b
mynssemM <mynssemgabriel@gmail.com>
2024-02-04 03:08:31 -0300

    Input added

--- src/index.php
+++ src/index.php
@@ -5,12 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/input.css">
 </head>
 <body>
     <div class ="container"> 
      <img src="img/mynssem.png" alt="hamburgueria">
      <h1>Mynssem's burguer</h1>
+     <div></div>
     </div>
+    <br>
+    <input type="text">
 </body>
 </html>
 
