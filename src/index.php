a42fbf220ef93d3b81892baac8a2b4d9a2517b1a
mynssemM <mynssemgabriel@gmail.com>
2024-02-17 23:42:41 -0300

    Include header responsevenss

--- src/index.php
+++ src/index.php
@@ -1,22 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>home</title>
+    <title>Home</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-    <pre>
-     <div class ="container"> 
-      <img src="img/mynssem.png" alt="hamburgueria">
-      <h1>Mynssem's burguer</h1>
-     </div>
-    </pre>
-    <br>
-    <a href="login.php">Fazer login</a>
 
+<body>
+    <header>
+        <div class="logoAndTitle">
+            <img src="img/mynssem.png" alt="hamburgueria">
+            <h1>Mynssem's burguer</h1>
+        </div>
+        <div class="socialmedia">
+            <a href="https://www.youtube.com"> instagram </a>
+            <a href="login.php" class="btn-link">Fazer login</a>
+        </div>
+    </header>
 </body>
+
 </html>
 
 <?php
--- src/login.php
+++ src/login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-</html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,22 +10,24 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    <pre>
-    <div class ="container"> 
-      <img src="img/mynssem.png" alt="hamburgueria">
-      <h1>Mynssem's burguer</h1>
-    </div>
-    </pre>
+    <header class="container">
+        <img src="img/mynssem.png" alt="hamburgueria">
+        <pre>
+         <h1>Mynssem's burger</h1>
+        </pre>
+    </header>
     <br>
-    <div class = "elemento">
-      <p>name</p>
-      <input type="text">
-      <p>password</p>
-      <input type="text">
+    <div class="elemento">
+        <p>Nome</p>
+        <input type="text">
+        <p>Senha</p>
+        <input type="text">
     </div>
-    
 </body>
+
 </html>
+
 
 <?php
