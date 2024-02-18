502084bec1ff721ce994e7a26d3eaade5fa8f365
mynssemM <mynssemgabriel@gmail.com>
2024-02-17 23:56:42 -0300

    splitting styles into files

--- src/login.php
+++ src/login.php
@@ -8,15 +8,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@1,300&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
 
 <body>
-    <header class="container">
-        <img src="img/mynssem.png" alt="hamburgueria">
-        <pre>
-         <h1>Mynssem's burger</h1>
-        </pre>
+    <header>
+        <div class="logoAndTitle">
+            <img src="img/mynssem.png" alt="hamburgueria">
+            <h1>Mynssem's burguer</h1>
+        </div>
     </header>
     <br>
     <div class="elemento">
